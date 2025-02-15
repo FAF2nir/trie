19a8cfb71cf2948f2e575acd6f3737e86d2e6029
FAF2nir <71982929+FAF2nir@users.noreply.github.com>
2025-02-15 20:54:37 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,6 @@
 
 int main()
 {
-    trie<int> t;
-    trie<int> s;
-    try {
-        std::cin >> t;
-    } catch(parser_exception e) {
-        std::cout << e.what() << std::endl;
-    }
-
-    std::cout << t << std::endl;
-    t.path_compress();
-    std::cout << t << std::endl;
-    /*
-    for(trie<int>::leaf_iterator it = t.begin(); it != t.end(); ++it) {
-        std::cout << *it << std::endl;
-    }
-    */
+    //insert here tests
+    return 0;
 }
